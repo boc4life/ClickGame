61b2bd548e7742c5128de9f812581b8b3ad9cffa
boc4life <lheandrew@gmail.com>
2019-04-06 15:57:57 -0400

    Updates

---
+++ precache-manifest.6958c339307b2fbfa63f7a875fda2ef6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d76327d9eefc62a77850",
+    "url": "/ClickGame/static/js/runtime~main.979c4616.js"
+  },
+  {
+    "revision": "b97c7e1715e4015d2cfc",
+    "url": "/ClickGame/static/js/main.761a64f9.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/ClickGame/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "b97c7e1715e4015d2cfc",
+    "url": "/ClickGame/static/css/main.65b3c567.chunk.css"
+  },
+  {
+    "revision": "e1bcaf1f8fe5fd48c6da6d273313fb03",
+    "url": "/ClickGame/index.html"
+  }
+];
